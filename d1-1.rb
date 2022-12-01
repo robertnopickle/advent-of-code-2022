4accfc37c12ffc90e31c5760c2410973356ac47a
Robert Garmhausen <81274344+robertnopickle@users.noreply.github.com>
2022-12-01 11:17:51 -0500

    day 1-1

---
+++ d1-1.rb
@@ -0,0 +1,9 @@
+file_data = File.read("d1-1.txt").split("\n\n")
+
+# go through each elf and sum the food item calories, creating an array of sums by elf
+summed_calories_by_elf = file_data.map do |elf|
+  elf.split("\n").map(&:to_i).sum
+end
+
+# output the largest sum in the array
+puts summed_calories_by_elf.sort!.last
