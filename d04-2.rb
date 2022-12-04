27cb1d70db9a4619d5ed5b996476001d67ef729a
Robert Garmhausen <81274344+robertnopickle@users.noreply.github.com>
2022-12-04 09:55:24 -0500

    day 4 part 2

---
+++ d04-2.rb
@@ -0,0 +1,17 @@
+section_assignment_pairs = File.read("input/d04-1.txt").split("\n")
+
+overlapping_total = 0
+
+section_assignment_pairs.each do |pair|
+  # make two-dimensional arrays of each section.  "1-3, 4-6" becomes [[1,2,3], [4,5,6]]
+  sections = pair.split(",")
+  sections.map! do |section|
+    section_edges = section.split("-")
+    (section_edges.first..section_edges.last).to_a
+  end
+  
+  # check if anything in one section is contained in the other using Set Intersection
+  overlapping_total += 1 if (sections.first & sections.last).any?
+end
+
+puts overlapping_total
