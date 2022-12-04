6bcfdaceeda753d9f511539c92cdcd3f9f9aa7c9
Robert Garmhausen <81274344+robertnopickle@users.noreply.github.com>
2022-12-04 10:07:57 -0500

    dry up this code a lil

--- d04-1.rb
+++ d04-1.rb
@@ -3,15 +3,15 @@ section_assignment_pairs = File.read("input/d04-1.txt").split("\n")
 overlapping_total = 0
 
 section_assignment_pairs.each do |pair|
-  # make arrays of each elf's sections.  "1-3" becomes [1,2,3]
-  (elf1, elf2) = pair.split(",")
-  elf1 = elf1.split("-")
-  elf2 = elf2.split("-")
-  elf1_sections = (elf1.first..elf1.last).to_a
-  elf2_sections = (elf2.first..elf2.last).to_a
+  # make two-dimensional arrays of each section.  "1-3, 4-6" becomes [[1,2,3], [4,5,6]]
+  sections = pair.split(",")
+  sections.map! do |section|
+    section_edges = section.split("-")
+    (section_edges.first..section_edges.last).to_a
+  end
 
   # using array subtraction to find if they overlap, example [1, 2] - [1, 2, 3] = []
-  overlapping_total += 1 if (elf1_sections - elf2_sections).empty? || (elf2_sections - elf1_sections).empty?
+  overlapping_total += 1 if (sections.first - sections.last).empty? || (sections.last - sections.first).empty?
 end
 
 puts overlapping_total
